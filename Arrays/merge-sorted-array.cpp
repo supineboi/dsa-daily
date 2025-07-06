18d4e2856ef122ae96609c229252b2231dfaafec
Mohd Rahil Khan <khanmohdrahil44@gmail.com>
2025-07-06 23:29:34 +0530

    Arrays: merge 2 sorted arrays [v1]

---
+++ Arrays/merge-sorted-array.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+using namespace std;
+
+/*
+  Merge two sort array such that 
+  resultant array also sort  
+*/
+
+int main(){
+    int arr1[] = {6, 19, 25, 50, 55};
+    int arr2[] = {4, 8, 21, 49, 53, 55, 56, 60};
+    
+    int m = sizeof(arr1)/sizeof(arr1[0]);
+    int n = sizeof(arr2)/sizeof(arr2[0]);
+
+    int arr3[m+n];
+
+    int i = 0, j = 0, k =0;
+
+    while(i<m || j<n){
+        if( i == m || j == n ){
+            if( i == m){
+                arr3[k] = arr2[j++];
+            }
+            else if( j == n){
+                arr3[k] = arr1[i++];
+            }
+            k++;
+            continue;
+        }
+        if(arr1[i] > arr2[j]){
+            arr3[k++] = arr2[j++];
+        }
+        else if(arr1[i] < arr2[j]){
+            arr3[k++] = arr1[i++];
+        }
+        else{
+            arr3[k++] = arr1[i++];
+            arr3[k++] = arr2[j++];
+        }
+    }
+
+    cout<<"Merge Sort Array is :: ";
+    for(int i=0; i<m+n;i++){
+        cout<<arr3[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
