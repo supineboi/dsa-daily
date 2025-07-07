f2b56f1ee3cef2246ed5b6b179eecd4322ef71f3
Mohd Rahil Khan <khanmohdrahil44@gmail.com>
2025-07-07 23:55:13 +0530

    Recursion: factorial using tail recusrion

--- Recursion/factorial-recursion.cpp
+++ Recursion/factorial-recursion.cpp
@@ -9,12 +9,21 @@ int getFactorial(int n){
     return n*getFactorial(n-1);
 }
 
+// Efficient Tail Recusrive Approach
+int tailRecursiveFactorial(int n, int k = 1){
+    if(n == 0 || n == 1){
+        return k;
+    }
+    return tailRecursiveFactorial(n-1, k*n);
+}
+
 int main(){
     int num;
     cout<<"Factorial of : ";
     cin>>num;
 
-    cout<<"Facotril of "<<num<<" is : "<<getFactorial(num);
+    cout<<"Factorial of "<<num<<" is : "<<getFactorial(num)<<endl;
+    cout<<"Factorial of "<<num<<" is (using tail recursive): "<<tailRecursiveFactorial(num)<<endl;
     
     return 0;
 }
